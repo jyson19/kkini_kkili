47fcfe33acb2a6f1b007a80856cfb2fe93b42fc1
jaewan004 <jaewany04@gmail.com>
2021-04-22 16:46:24 +0900

    [mod] notice 경로 수정

---
+++ src/main/java/com/kk/dao/NoticeDAOImpl.java
@@ -0,0 +1,15 @@
+package com.kk.dao;
+
+import java.util.List;
+
+import com.kk.domain.NoticeVO;
+
+public class NoticeDAOImpl implements NoticeDAO{
+
+	@Override
+	public List<NoticeVO> getNoticeList(NoticeVO vo) {
+		
+		return null;
+	}
+	
+}
