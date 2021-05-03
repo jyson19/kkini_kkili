0ce64826d4cb8b1d6b0aa298dd98de3beb17cc50
Easycelcius <ji.sub.9411@gmail.com>
2021-05-03 15:21:40 +0900

    [mod] 출력문 로그로 수정 중 (관리자/기본 컨트롤러 수정 완료)

--- src/main/java/com/kk/controller/BasicController.java
+++ src/main/java/com/kk/controller/BasicController.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.kk.domain.MemberVO;
-import com.kk.domain.PageMaker;
 import com.kk.domain.PagingCriteria;
 import com.kk.service.ContactService;
 import com.kk.service.ProfileService;
@@ -23,6 +21,8 @@ import com.kk.service.ProfileService;
 @RequestMapping("main")
 public class BasicController {
 
+	private Logger log = LoggerFactory.getLogger(AdminController.class);
+	
 	@Autowired
 	private ContactService contactService;
 	
@@ -32,7 +32,7 @@ public class BasicController {
 	// 관련 페이지로 이동
 	@RequestMapping("{step}.do")
 	public String movingPage(@PathVariable("step") String step) {
-		System.out.println("BasicController : " + step + "페이지로 이동 요청");
+		log.info("BasicController : " + step + "페이지로 이동 요청");
 		return "main/" + step;
 	}
 
@@ -40,14 +40,12 @@ public class BasicController {
 	@RequestMapping("r/contactList.do")
 	@ResponseBody
 	public List<Map<String, String>> mainContactList(Model model) {
-		System.out.println("BasicController.mainContactList");
+		log.info("BasicController.mainContactList");
 
 		PagingCriteria cri = new PagingCriteria();
 
 		cri.setPageNum(1);
 		cri.setAmount(10);
-
-		System.out.println(cri);
 
 		List<Map<String, String>> boardList = contactService.getMainContactList(cri);
 
@@ -58,7 +56,7 @@ public class BasicController {
 	@RequestMapping("r/profileList.do")
 	@ResponseBody
 	public List<HashMap<String, String>> mainProfileList() {
-		System.out.println("BasicController.mainProfileList");
+		log.info("BasicController.mainProfileList");
 		return profileService.getMainHostInfo();
 	}
 
